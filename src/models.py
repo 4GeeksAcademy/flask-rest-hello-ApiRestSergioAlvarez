8047bc0c19c7ff445e8648892d4e02c4120315c5
Sergioalv15 <sergioalv961@gmail.com>
2025-10-14 10:14:26 +0000

    [Proyecto terminado]

--- src/models.py
+++ src/models.py
@@ -1,19 +1,65 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ ='user'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorites_characters_list: Mapped[list['FavCharacters']] = relationship(back_populates='user')
+    favorites_starships_list: Mapped[list['FavStarships']] = relationship(back_populates='user')
+    favorites_planets_list: Mapped[list['FavPlanets']] = relationship(back_populates='user')
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    height: Mapped[int] = mapped_column(Integer)
+    weight: Mapped[int] = mapped_column(Integer)
+    favorites : Mapped[list['FavCharacters']] = relationship(back_populates='people')
+    
+class Starships(db.Model):
+    __tablename__ = 'starships'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    height: Mapped[int] = mapped_column(Integer)
+    weight: Mapped[int] = mapped_column(Integer)
+    favorites : Mapped[list['FavStarships']] = relationship(back_populates='starships_list')
+    
+class Planets(db.Model):
+    __tablename__ = 'planets'    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    height: Mapped[int] = mapped_column(Integer)
+    weight: Mapped[int] = mapped_column(Integer)
+    favorites : Mapped[list['FavPlanets']] = relationship(back_populates='planets')
+    
+class FavCharacters(db.Model):
+    __tablename__ = 'fav_characters'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    character_id: Mapped[int] = mapped_column(ForeignKey('characters.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped[list['User']] = relationship(back_populates='favorites_characters_list')
+    people: Mapped[list['Characters']] = relationship(back_populates='favorites')
+    
+class FavPlanets(db.Model):
+    
+    __tablename__ = 'fav_planets'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    planet_id: Mapped[int] = mapped_column(ForeignKey('planets.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    planets: Mapped[list['Planets']] = relationship(back_populates='favorites')
+    user: Mapped[list['User']] = relationship(back_populates='favorites_planets_list')
+
+class FavStarships(db.Model):
+    __tablename__ = 'fav_starships'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    starships_id: Mapped[int] = mapped_column(ForeignKey('starships.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped[list['User']] = relationship(back_populates='favorites_starships_list')
+    starships_list: Mapped[list['Starships']] = relationship(back_populates='favorites')
